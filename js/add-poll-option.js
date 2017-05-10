6c8530fbb5dc50fa86505fd24244b834d5142dd4
Victoria Galvan <VGALVAN@ROLLINS.EDU>
2017-05-09 21:49:55 -0400

    Add files via upload

---
+++ js/add-poll-option.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    var formElement = "<input class='form-input' type='text' placeholder='Option'><br>";
+
+    $("#add-option").click( function() {
+        alert( "Functionality coming soon." );
+    });
+
+});
